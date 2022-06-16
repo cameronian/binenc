556e6d4d2456fdaf002aa239f71772ec6c4b97c7
Ian <cameronian0@protonmail.com>
2022-06-16 23:45:36 +0800

    initial

--- lib/binenc.rb
+++ lib/binenc.rb
@@ -7,8 +7,14 @@ require_relative "binenc/version"
 
 require_relative 'binenc/provider'
 
+require_relative 'binenc/engine_factory'
+
+require_relative 'binenc/binary_object'
+
 
 module Binenc
   class Error < StandardError; end
+
+  class BinencEngineException < StandardError; end
   # Your code goes here...
 end
