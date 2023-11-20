4af86ec67246046b4b81b235b4f65dbb875d5b8b
Chris <chris@antrapol.com>
2023-11-20 23:27:24 +0800

    Fix bug

--- lib/binenc/bin_tag/bin_tag.rb
+++ lib/binenc/bin_tag/bin_tag.rb
@@ -21,44 +21,51 @@ module Binenc
       # DSL part
       #
       def define_constant(key, val, parent = nil, &block)
-        if @constant.keys.include?(key) 
-          if @constRaiseIfDup
-            raise BinTagConstantKeyAlreadyExist, "Constant key '#{key}' already exist"
-          else
-            logger.warn "Overwriting existing key '#{key}'..."
+        if not_empty?(key)
+          ikey = key.to_s.downcase
+          if @constant.keys.include?(ikey) 
+            if @constRaiseIfDup
+              raise BinTagConstantKeyAlreadyExist, "Constant key '#{key}' already exist"
+            else
+              logger.warn "Overwriting existing key '#{key}'..."
+            end
           end
+
+          if val.is_a?(String) and val =~ /#/
+            val = val.gsub("#",@parent).strip
+          end
+
+          @constant[ikey] = val
+
+          oldParent = @parent 
+
+          @parent = val
+          if block
+            instance_eval(&block)
+          end
+          @parent = oldParent
         end
-
-        if val.is_a?(String) and val =~ /#/
-          val = val.gsub("#",@parent).strip
-        end
-
-        @constant[key] = val
-
-        oldParent = @parent 
-
-        @parent = val
-        if block
-          instance_eval(&block)
-        end
-        @parent = oldParent
 
       end
 
       def constant_value(key)
-        @constant[key]
+        if not_empty?(key)
+          #logger.debug "search key : #{key}"
+          #logger.debug "Record : #{@constant}"
+          @constant[key.to_s.downcase]
+        end
+        #@constant[key.to_s.downcase] if not_empty?(key)
       end
 
+      # find constant from given value
       def value_constant(val)
-        #if TR::RTUtils.on_java?
-          if val.is_a?(Integer)
-            @constant.invert[val.to_s.to_i]
-          else
-            @constant.invert[val]
-          end
-        #else
-        #  @constant.invert[val]
-        #end
+        if val.is_a?(Integer)
+          v = @constant.invert[val.to_s.to_i]
+        else
+          v = @constant.invert[val]
+        end
+        v.to_sym if not_empty?(v)
+        #v if not_empty?(v)
       end
 
       def raise_on_constant_key_duplicate=(val)
